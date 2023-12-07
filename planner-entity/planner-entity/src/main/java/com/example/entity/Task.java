f36c42595ae82162579bdafb5d9e811d4b7bd04d
Alexey Tarabanov <tarabanov@bk.ru>
2023-12-08 01:39:28 +0300

    add task

--- planner-entity/planner-entity/src/main/java/com/example/entity/Task.java
+++ planner-entity/planner-entity/src/main/java/com/example/entity/Task.java
@@ -18,7 +18,7 @@ import java.util.Objects;
  * */
 
 @Entity
-@Table(name = "task", schema = "users", catalog = "planner_users")
+@Table(name = "task", schema = "todo", catalog = "planner_todo")
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
